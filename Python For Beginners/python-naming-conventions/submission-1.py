a30b45448f407f986fa4ad34898e6118824e3ae5
tejeshrem <tejesh.remuduru@gmail.com>
2026-05-31 18:28:55 -0700

    Add: python-naming-conventions - submission-1

---
+++ Python For Beginners/python-naming-conventions/submission-1.py
@@ -0,0 +1,7 @@
+python_Creation_Date = "February, 1991"
+java_Creation_Date = "May, 1995"
+javascript_Creation_Date = "December, 1995"
+
+print(python_Creation_Date)
+print(java_Creation_Date)
+print(javascript_Creation_Date)

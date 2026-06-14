16e20fe568d8f5e80c2ac82cdf2b752c9f356174
tejeshrem <tejesh.remuduru@gmail.com>
2026-06-13 17:12:31 -0700

    Add: populating-next-right-pointers-in-each-node - submission-1

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-1.py
@@ -0,0 +1,51 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        '''
+
+        BFS - stack 
+        level by level traversal 
+            add left, then right neighbor 
+        
+        for each level 
+        
+        pop node from stack 
+        if stack.peek() make that the node's next
+        if not , continue with the next ptr as null 
+        
+        return the root 
+
+        '''
+        if not root:
+            return root
+        # init deque 
+        q = deque([root])
+
+
+        # lvl by lvl traversal
+        while q:
+            k = len(q)
+
+            while k:
+                node = q.popleft()
+                
+                k -=1 
+                # assign next field 
+                if q and k > 0:
+                    node.next = q[0]
+                if node.right and node.left:
+                    q.append(node.left)
+                    q.append(node.right)
+                
+
+        return root
+        # return root 
